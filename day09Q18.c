6070a918e388f8aca53e87fde2e978183babfb9d
prachi-narang <prachinarang125@gmail.com>
2026-08-19 23:52:47 +0530

    added a program that accepts a percentage(0-100) and assigns a grade

---
+++ day09Q18.c
@@ -0,0 +1,22 @@
+
+//write a program the acceptsna percentage(0-100) and assigns a grade based on the followings criteria:
+#include <stdio.h>
+int main(){
+float percentage;
+printf("enter percentage: ");
+scanf("%f", &percentage);
+if(percentage>=90 && percentage<=100)
+printf("Grade A");
+else if (percentage>=80)
+printf("Grade B");
+else if(percentage>=70)
+printf("Grade C");
+else if(percentage>=60)
+printf("Grade D");
+else if(percentage>=0)
+printf("Grade F");
+else
+printf("invalid percentage");
+return 0;
+}
+
