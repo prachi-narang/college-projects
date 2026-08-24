ed4ab130cea8e2bc30a6888e89acab78249a1828
prachi-narang <prachinarang125@gmail.com>
2026-08-24 22:26:47 +0530

    added a program tocalculate the facorial of a number

---
+++ day15Q29.c
@@ -0,0 +1,20 @@
+//write a program to calculate the factorial of a number.
+#include <stdio.h>
+int main(){
+int n,i;
+unsigned long factorial = 1;
+
+printf("Enter a number: ");
+scanf("%d", &n);
+
+if(n<0){
+printf("Factorial of a negative number doesnot exist.\n");
+}
+else{
+for(i = 1; i <= n;i++){
+factorial = factorial * i;
+}
+printf("Factorial of %d=%lu\n", n, factorial);
+}
+return 0;
+}
