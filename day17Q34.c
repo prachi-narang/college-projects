02362b358d7e9f070064706f320a5672d73baa37
prachi-narang <prachinarang125@gmail.com>
2026-08-26 12:43:28 +0530

    added a program to check if a number is [prime

---
+++ day17Q34.c
@@ -0,0 +1,27 @@
+//write a program to check if a number is prime.
+#include <stdio.h>
+int main(){
+int n,i;
+int flag = 0;
+printf("Enter a number: ");
+scanf("%d", &n);
+
+if (n<=1){
+flag = 1;
+}
+else{
+for(i = 2; i<= n/2; i++)
+{
+if (n %i == 0){
+flag = 1;
+break;
+}
+}
+}
+if(flag == 0)
+printf("%d is a prime number", n);
+else
+printf("%d is not a prime number.", n);
+
+return 0;
+}
