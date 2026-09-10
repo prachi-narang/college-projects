5e7abb53b677f4087a9b0eb142ca0242dccaaf43
prachi-narang <prachinarang125@gmail.com>
2026-09-10 12:31:56 +0530

    added a program digits occurs most times in an integer number

---
+++ day32Q64.c
@@ -0,0 +1,29 @@
+//find  the digit that occurs the mmost times in an integer number
+#include<stdio.h>
+
+int main()
+{
+long int n;
+int count[10] = {0};
+int digit, max=0,ans=0;
+
+printf("Enter a number: ");
+scanf("%ld", &n);
+while (n>0)
+{
+digit = n%10;
+count[digit]++;
+n = n/10;
+}
+for(int i = 0; i<10; i++)
+{
+if(count[i]>max)
+{
+max = count[i];
+ans = i;
+}
+}
+printf("most frequent digit = %d", ans);
+return 0;
+}
+
