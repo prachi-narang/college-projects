7a8423b663cb3da4cf4c8d5ccac5260e1c2db0c3
prachi-narang <prachinarang125@gmail.com>
2026-09-19 22:41:36 +0530

    added a program Count characters in a string without using built-in length functions.

---
+++ day41Q81.c
@@ -0,0 +1,19 @@
+//Count characters in a string without using built-in length functions.#include <stdio.h>
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    int count = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[count] != '\0' && str[count] != '\n')
+    {
+        count++;
+    }
+
+    printf("Number of characters = %d", count);
+
+    return 0;
+}
