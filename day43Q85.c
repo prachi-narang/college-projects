d858f0b3b545aed5fa02fe4f7446508da05c5e4c
prachi-narang <prachinarang125@gmail.com>
2026-09-22 19:49:23 +0530

    added a program to reverse the string

---
+++ day43Q85.c
@@ -0,0 +1,23 @@
+//reverse a string.
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, length = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    // Find length of string
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    // Print in reverse
+    printf("Reversed string: ");
+    for (i = length - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+
+    return 0;
+}
