7957644af97c5c5bca68edfc8426731c2f99554f
prachi-narang <prachinarang125@gmail.com>
2026-09-23 21:44:05 +0530

    added a program to count frequency of given character

---
+++ day45Q89.c
@@ -0,0 +1,24 @@
+//Count frequency of a given character in a string.
+#include <stdio.h>
+
+int main()
+{
+    char str[100], ch;
+    int i, count = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    printf("Enter character: ");
+    scanf(" %c", &ch);   // %c se pehle space
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] == ch)
+            count++;
+    }
+
+    printf("Frequency of %c = %d", ch, count);
+
+    return 0;
+}
